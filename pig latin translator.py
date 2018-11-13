f02073ee498c4a4ff93d9e6104221fa9182eb758
heavilyarmedclown <43704663+heavilyarmedclown@users.noreply.github.com>
2018-11-12 21:25:09 -0500

    Update pig latin translator.py

--- pig latin translator.py
+++ pig latin translator.py
@@ -1,11 +1,11 @@
-pyg = 'ay'
+pyg = 'ay' 
 
-original = raw_input('Enter a word:')
-
-if len(original) > 0 and original.isalpha():
-  word = original.lower()
-  first = word[0]
-  new_word = word[1:len(new_word)] + first + pyg
+original = raw_input('Enter a word:') 
+#ask for user input
+if len(original) > 0 and original.isalpha(): #test for input and only letters
+  word = original.lower() #returns var as lowercase string
+  first = word[0] #isolates first letter
+  new_word = word[1:len(new_word)] + first + pyg #translation
   print new_word
 else:
-  print 'empty'
+  print 'empty' #no user input or incorrect variables
