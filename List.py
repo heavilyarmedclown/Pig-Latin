0e3aaed47f37af263598cef21a1afe61c73a598f
heavilyarmedclown <43704663+heavilyarmedclown@users.noreply.github.com>
2018-11-14 17:17:27 -0500

    Indexing and Modifying Arrays

---
+++ List.py
@@ -0,0 +1,8 @@
+friends = ["Kevin" , "Karen" , "Karl"]
+print(friends[0])
+print(friends[-3])
+print(friends[1:])
+longrange = ["kevin" , "karen" , "karl" , "kody" , "kandy"]
+print(longrange[1:3])
+longrange[1] = "kora"
+print(longrange[1])
