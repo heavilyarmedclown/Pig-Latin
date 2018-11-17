f968640d19bea3d07b1c63b991485bd4f72a692e
heavilyarmedclown <43704663+heavilyarmedclown@users.noreply.github.com>
2018-11-17 11:02:48 -0500

    Update translator.py

--- translator.py
+++ translator.py
@@ -16,5 +16,11 @@ def translate(phrase):
         else:
             translation = translation + letter
     return translation
+#above function uses for loop to run through each element in phrase variable (in this case its a letter)
+#each loop through phrase variable checks to see if the letter is a vowel (first if loop)
+#if its a vowel the translation variable adds a 'G' to its string
+#additional if/else loop at this point to distinguish capital/lower case and preserve
+#final else returns the original letter parsed if it was not a vowel
 
 print(translate(input("Enter a phrase: ")))
+#prints the translate function with the user input as the phraase variable defined in the function
